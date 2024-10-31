85aa332851ae66902979108cff991ed63cb5e253
Aleksei Zolotilin <az025971@tite.amiedu-ict.net>
2024-10-31 10:44:37 +0200

    init

---
+++ sqlite_connection.py
@@ -0,0 +1,8 @@
+# python -m venv venv
+# .\venv\Scripts\activate
+import sqlite3
+from sqlite3 import Error
+try:
+    con = sqlite3.connect("./databases/movies.db")
+except Error as e:
+    print(e)
